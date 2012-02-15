b006d7afbb03b51f61bdd51ca21b87669de63106
unknown <feifan@feifan-x220.(none)>
2012-02-14 20:21:03 -0800

    Add test for modification in <cache.py> CacheConnectionPool. Supporting multi-cache backends.

--- tests/views.py
+++ tests/views.py
@@ -1,8 +1,11 @@
-from django.core.cache import get_cache
+from django.core.cache import get_cache, cache
 from django.http import HttpResponse
-
+#test with django 1.3.1 by feifan 2/14/2012
 
 def someview(request):
-    cache = get_cache('redis_cache.cache://127.0.0.1')
+    #cachex = get_cache('redis_cache.cache://127.0.0.1')
     cache.set("foo", "bar")
-    return HttpResponse("Pants")
+    cache2 = get_cache('2ndcache')
+    cache2.set('foo', 'bar2') #shold use db 14, not overwirte foo in default db15
+
+    return HttpResponse("should be bar and bar2 in next line:<br>%s %s" % (cache.get('foo'), cache2.get('foo')))
