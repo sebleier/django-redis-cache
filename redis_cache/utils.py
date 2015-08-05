26e67cd4aebf92baa65b2051ef1fbb0f7f25a38a
Carlton Gibson <carlton.gibson@noumenal.co.uk>
2015-08-05 09:44:36 +0200

    Correct reference to `import_module`

--- redis_cache/utils.py
+++ redis_cache/utils.py
@@ -57,7 +57,7 @@ def get_servers(location):
 def import_class(path):
     module_name, class_name = path.rsplit('.', 1)
     try:
-        module = import_module(module_name)
+        module = importlib.import_module(module_name)
     except ImportError:
         raise ImproperlyConfigured('Could not find module "%s"' % module_name)
     else:
