88cd5824cb8019a133bd67fb8ca333020752fb1e
Tim Graham <timograham@gmail.com>
2016-08-26 09:17:14 -0400

    Confirm support for Python 3.5

--- setup.py
+++ setup.py
@@ -14,6 +14,7 @@ setup(
         "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 3.3",
         "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
         "Operating System :: OS Independent",
         "Topic :: Software Development :: Libraries",
         "Topic :: Utilities",
