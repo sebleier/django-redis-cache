a2d03d02f3fd9b70948e21f04fbb94d70d069642
Sean Bleier <sbleier@myriad.com>
2020-10-14 17:01:41 -0700

    I guess we can't do Python 3.9 in travis yet.

--- setup.py
+++ setup.py
@@ -15,7 +15,6 @@ setup(
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: 3.9",
         "License :: OSI Approved :: BSD License",
         "Operating System :: OS Independent",
         "Topic :: Software Development :: Libraries",
