240bbc1f6de555e44854728ce2f25376a40d3cdd
Aleksandr Shtaub <91491081+aleksandr-shtaub@users.noreply.github.com>
2024-01-29 16:39:51 +0200

    Update redis repo link

--- install_redis.sh
+++ install_redis.sh
@@ -2,6 +2,6 @@
 
 : ${REDIS_VERSION:="4.0.11"}
 
-test -d redis || git clone https://github.com/antirez/redis
+test -d redis || git clone https://github.com/redis/redis
 git -C redis checkout $REDIS_VERSION
 make -C redis
